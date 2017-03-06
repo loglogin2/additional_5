38655376255ce3aa2c2271bc61f50e8925399a59
loglogin2 <i9991559@gmail.com>
2017-03-06 19:04:00 +0300

    Brackets/feature: basic implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    if ( str.length == 0 || str.length % 2 == 1){
+        return false;
+    }
+    var open = [];
+    var close = [];
+    var arr = [];
+    for ( var  i = 0 ; i < bracketsConfig.length ; i ++ ){
+        open.push(bracketsConfig[i][0]);
+        close.push(bracketsConfig[i][1]);
+    }
+    for ( var i = 0 ; i < str.length ; i ++ ){
+        if ( open.indexOf(str[i]) >= 0 ){
+            arr.push(str[i]);
+        }
+        if ( close.indexOf(str[i]) >= 0){
+            if ( close.indexOf(str[i]) == open.indexOf(arr[arr.length-1])){
+                arr.pop();
+            }
+        }
+    }
+    return arr.length == 0 ;
 }
